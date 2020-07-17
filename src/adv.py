642c28c990afb91ee3360f0b60b8ba603508eeb4
Aaron Williams <aaronrw4@yahoo.com>
2020-07-16 18:29:10 -0700

    Created items dictionary

--- src/adv.py
+++ src/adv.py
@@ -2,28 +2,36 @@ from room import Room
 from player import Player
 from item import Item
 
+#Declare all items
+
+items = {
+    'rock': Item('Rock', 'Fist size piece of granite.'),
+    'lamp': Item('Lamp', 'Lamp with plenty of fuel.'),
+    'sword': Item('Sword', 'A weapon for fighting monsters.'),
+    'key': Item('Key', 'An old key, but what to?')
+}
+
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
                      "North of you, the cave mount beckons",
-                     (Item('Rock', 'Fist size piece of granite.'))),
+                     (items['rock'])),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east.""", ()), 
+passages run north and east.""", (items['lamp'])), 
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm.""", ()),
+the distance, but there is no way across the chasm.""", (items['sword'])),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air.""", ()),
+to north. The smell of gold permeates the air.""", (items['key'])),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south.""", ()),
 }
-
 
 # Link rooms together
 
